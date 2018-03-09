47739a8001061fff11bb53788726401d65b069b4
Mahdi Ben Alaya <benalaya.mahdi@gmail.com>
2018-03-09 09:08:24 -0600

    Update start.bat

--- onem2m-platform/start.bat
+++ onem2m-platform/start.bat
@@ -1,1 +1,1 @@
-java -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true plugins/org.eclipse.equinox.launcher_*.jar -console -noExit
+java -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true plugins/org.eclipse.equinox.launcher_1.4.0.v20161219-1356.jar -console -noExit

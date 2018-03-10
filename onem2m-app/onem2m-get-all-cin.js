42225888d3fe193058c5a31ddf907fa0678c7dca
Mahdi Ben Alaya <benalaya.mahdi@gmail.com>
2018-03-09 18:10:12 -0600

    Create onem2m-get-all-cin.js

---
+++ onem2m-app/onem2m-get-all-cin.js
@@ -0,0 +1,28 @@
+var bodyParser = require('body-parser');
+var request = require('request');
+
+console.log("\n▶▶▶▶▶");
+var originator = "Cae-admin";
+var method = "GET";
+var uri= "http://10.24.46.148:8080/server/ae_oneT/CarPark?rcn=4";
+
+console.log(method+" "+uri);
+
+var options = {
+	uri: uri,
+	method: method,
+	headers: {
+		"X-M2M-Origin": originator,
+		"Content-Type": "application/json"
+	}
+};
+
+request(options, function (error, response, body) {
+	console.log("◀◀◀◀◀");
+	if(error){
+		console.log(error);
+	}else{
+		console.log(response.statusCode);
+		console.log(body);
+	}
+});
